1bd44ec877f460c7d4c29404c39a7932deefa16a
Anh Khoa <khoahoc72@gmail.com>
2024-06-17 23:41:07 +0700

    Deploy front end

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://g-weather-forecast-front-end.vercel.app',
+    credentials: true,
+  });
 
   await app.listen(3000);
 }
