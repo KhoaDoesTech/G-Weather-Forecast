c588510778a566b0ec66715f50db4bf6e4802142
Anh Khoa <khoahoc72@gmail.com>
2024-06-20 10:46:00 +0700

    Fix CORS Backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'https://g-weather-forecast-front-end.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
