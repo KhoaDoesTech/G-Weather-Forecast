9f5fff9e42603e3e3bc810bd49890c5e3a3248dc
Anh Khoa <khoahoc72@gmail.com>
2024-06-20 10:20:27 +0700

    Fix CORS

--- frontend/src/apis/User/index.js
+++ frontend/src/apis/User/index.js
@@ -2,17 +2,7 @@ import { apiInstance } from '..';
 
 export const postSubscribe = async (email, city) => {
   try {
-    const response = await apiInstance.post(
-      `/user/register`,
-      { email, city },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin':
-            'https://g-weather-forecast-front-end.vercel.app',
-        },
-      }
-    );
+    const response = await apiInstance.post(`/user/register`, { email, city });
     return response.data;
   } catch (error) {
     console.error('Error fetching weather data', error);
