77e2c49f88794324415e8b5bdb4410fb1fb7fd09
Anh Khoa <khoahoc72@gmail.com>
2024-06-20 10:25:10 +0700

    Fix CORS

--- frontend/src/apis/User/index.js
+++ frontend/src/apis/User/index.js
@@ -2,7 +2,15 @@ import { apiInstance } from '..';
 
 export const postSubscribe = async (email, city) => {
   try {
-    const response = await apiInstance.post(`/user/register`, { email, city });
+    const response = await apiInstance.post(
+      `/user/register`,
+      { email, city },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
     return response.data;
   } catch (error) {
     console.error('Error fetching weather data', error);
